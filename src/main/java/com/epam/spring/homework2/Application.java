aac48f511d9a76c0e8ec570aa00397acaef43495
Olesia Trusova <trusova.olesia@gmail.com>
2021-11-11 13:42:30 +0200

    Application

--- src/main/java/com/epam/spring/homework2/Application.java
+++ src/main/java/com/epam/spring/homework2/Application.java
@@ -4,7 +4,7 @@ import com.epam.spring.homework2.config.BeanConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class application {
+public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
         String[] allBeanNames = context.getBeanDefinitionNames();
